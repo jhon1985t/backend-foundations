af8bc6e2a6460d7017ac1bafa79113cd84057349
jhonjto <jhon1985t@gmail.com>
2026-01-07 17:54:44 -0500

    update flow creation users in db

--- app/db.py
+++ app/db.py
@@ -11,3 +11,7 @@ class Base(DeclarativeBase):
 engine = create_engine(settings.database_url, echo=False)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Importar todos los modelos para registrarlos en Base.metadata
+# DEBE ocurrir DESPUÉS de definir Base, pero ANTES de create_all
+from app.users.models import User  # noqa: F401, E402
